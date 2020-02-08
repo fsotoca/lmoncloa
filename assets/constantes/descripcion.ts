a84169d4959fa3decc493953b992a22dca071f7b
Fran Sotoca <fsotocavsf@gmail.com>
2020-02-08 10:08:02 +0100

    subida prod git

---
+++ assets/constantes/descripcion.ts
@@ -0,0 +1,66 @@
+export const curriculumvdir = `
+Siendo muy joven colabora con todas las compañías privadas dedicadas al género lírico,
+siendo dirigido por los mejores directores, y destacándose por sus cualidades
+interpretativas.
+Todo ello le lleva a volcar su bagaje profesional sobre un género que conoce
+profundamente al pertenecer a una familia de tres generaciones de dedicación completa
+a él. Así en 2004 debuta dirigiendo “La del Manojo de rosas” en la temporada lírica de
+Huelva, con un rotundo éxito.
+A este título le seguirán hasta más de veinticinco títulos de repertorio así como el
+reestreno de “El Molinero de Subiza”.
+Crea un Homenaje al maestro Chapí, que se estrena en el teatro El Bosque de Móstoles,
+en el cual se alaba su habilidad para alternar medios audiovisuales con la sinfonía de los
+Gnomos del Maestro Chapí.
+Le sigue la creación de conciertos líricos tematizados en el Teatro Amaya de Madrid,
+como el Zoo de la lírica o La Zarzuela + divertida de los cuales se hacen más de 40
+representaciones.
+Dirige el Festival de Zarzuela de Canarias durante tres temporadas.
+La mano acertada de LM llenó de ritmo la acción con algunos números muy próximos
+al género del musical.- Ópera Actual
+Todo el montaje circuló dentro de las premisas del directo lo que provocó el rápido
+guiño de complicidad con el público, en una acción bastante dinámica...- Ópera Actual
+Ha sido uno de los mejores espectáculos del año.- Canarias 7
+Crea y dirige el espectáculo “Historia de nuestra Zarzuela”.
+Dirige para el festival de ópera de Medellín (Colombia) títulos como “Le nozze di
+Figaro”, “La traviata”, “Carmen”, etc.
+Es director y coautor de musicales como “Leyendas de Bécquer”, sobre la obra del autor
+romántico, y “Una noche en Las Vegas”.
+Dirige “El paraíso de los niños” para el Teatro de la Zarzuela de Madrid.
+Dramatiza y escenifica “Carmina Burana” y “Requiem por Mozart” con la Coral
+Polifónica de Getafe.
+Dirige y actúa en el espectáculo “Tenorissimus” con el que se han hecho más de 100
+representaciones y han visto 25.000 espectadores, estando 3 temporadas seguidas en
+Madrid.`;
+
+export const curriculum = `Siendo muy joven colabora con todas las compañías privadas dedicadas al género lírico.
+Es contratado por D. José Tamayo para su espectáculo “Antología de la Zarzuela”.
+En el campo de los musicales debuta con “Los Miserables” en el teatro Nuevo Apolo de
+Madrid. Es “Tony” en “West Side Story” por el que se le concede el premio al joven actor
+más valorado.
+También “Chicago” y en el exitoso musical español “Estamos en el aire”. Protagoniza para
+TVE “Yola”.
+L. M. por su parte les da la réplica a ambos con una caracterización travestida de una periodista del corazón que alcanza
+unos agudos en falsete dignos del mismo Farinnelli. Si en West Side Story, la temporada pasada, había mostrado su
+lirismo y sus excelentes habilidades vocales, aquí ofrece una demostración de su habilidad para la comedia.- Diario 16
+En el Teatro de la Zarzuela actúa habitualmente siendo su última producción la ópera de
+Sorozábal “Juan José”. Dentro de los Veranos de la Villa “La Gran Vía”. En Opera de
+Lausanne “Pan y toros” y en el Stadt Theater de St. Gallen “Doña Francisquita”. También “El
+Barberillo de Lavapies”, “Luisa Fernanda”, “Los Gavilanes”, “La verbena de la Paloma”…
+hasta más de 40 títulos así como los homenajes al Maestro Chueca y al Maestro Alonso.
+Fue un lujo L.M. en Cardona. Un auténtico tenor que dio dignidad y señorío al personaje y sobre todo calidad vocal...
+Levante Marzo- 2003
+En la zarzuela de Serrano (Los Claveles) cabe hablar de la prestación excelente por empaque, carácter del personaje y
+asimismo por modos vocales de L. M. Mundoclásico.com 2008
+En Ópera interpreta roles coprimarios en “Salome”, “Madame Butterfly”, “Traviata”,
+“Rigoletto”, “Otello”, etc. Y Canta como primer tenor el estreno en España de “Curlew
+River”. En teatros como Campoamor de Oviedo, Palacio Euskalduna de Bilbao, Festival de
+Ópera de Tenerife, Pérez Galdós de Las Palmas, etc.
+Mención especial merecen los hermanos L. y M. Moncloa, dotados de una solvencia vocal y teatral admirable.
+Operaworld 2015
+… y toques de genialidad en L. M. Noticiasdigital.es 2016
+Tenor invitado en los eventos para la Conmemoración del 2 de Mayo, dirigido por La fura del
+Baus, cantando ante más de medio millón de personas.
+Dentro del repertorio de oratorio distintas Misas de Mozart y los Réquiem de Mozart y
+Donizetti.
+Ejerce de tenor y maestro de ceremonias en los espectáculos “Tenorissimus” y “Entre tenores”
+vistos por más de 20.000 espectadores.`;
